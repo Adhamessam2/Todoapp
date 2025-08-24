4bef668750c9817776d530d8ffe0b330c9667a6e
Soliman Ragab <git@github.com:solimanragab11>
2025-08-24 15:05:06 +0300

    dddf

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todoapp/core/data/firebase_functions.dart';
+import 'package:todoapp/core/models/todo_model.dart';
 import 'package:todoapp/features/splash_screen/splash.dart';
+import 'package:todoapp/firebase_options.dart';
 
 void main() {
   runApp(Todoapp());
@@ -15,9 +20,6 @@ class Todoapp extends StatefulWidget {
 class _TodoappState extends State<Todoapp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Splashscreen(),
-      debugShowCheckedModeBanner: false,
-    );
+    return MaterialApp(home: Splashscreen(), debugShowCheckedModeBanner: false);
   }
 }
